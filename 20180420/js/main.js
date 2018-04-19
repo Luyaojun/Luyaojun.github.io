720bec8c092140af420bd0a15eb14f9aa84d53fb
SunLuyao <1786526136@qq.com>
2018-04-20 01:35:19 +0900

    update

--- 20180420/js/main.js
+++ 20180420/js/main.js
@@ -5,15 +5,22 @@ $(document).ready(function () {
        $("a.nav-link").removeClass("active");
        $(this).addClass("active");
    });
-    $("#Videos").on("click",function () {
+    $("#Videos").on("click", function () {
         $("#media_container").css("display","block");
         $("#books_container").css("display","none");
     });
-    $("#Books").on("click",function () {
+    $("#Books").on("click", function () {
         $("#media_container").css("display","none");
         $("#books_container").css("display","block");
     });
 
+    $(".list-group-item").on("click", function () {
+        $(".list-group-item").removeClass("active");
+        $(this).addClass("active");
+        // if (this.innerHTML == "THE TCP/IP GUIDE"){
+        //     window.location.href = "http://index-of.es/Magazines/hakin9/books/No.Starch.TCP.IP.Guide.Oct.2005.pdf";
+        // }
+    });
     //var video_player = videojs("#Video-bar");
     //var player_width = $(".col-md-12").css("width");
     //video_player.width();
