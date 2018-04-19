73b118cd9915ff88e2d319797213c6b79a175927
SunLuyao <1786526136@qq.com>
2018-04-20 00:21:01 +0900

    update

--- 20180420/js/main.js
+++ 20180420/js/main.js
@@ -7,8 +7,14 @@ $(document).ready(function () {
    });
     $("#Videos").on("click",function () {
         $("#media_container").css("display","block");
+        $("#books_container").css("display","none");
     });
     $("#Books").on("click",function () {
         $("#media_container").css("display","none");
+        $("#books_container").css("display","block");
     });
+
+    var video_player = videojs("#Video-bar");
+    var player_width = $(".col-md-12").css("width");
+    video_player.width(player_width);
 });
